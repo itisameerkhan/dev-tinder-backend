fc0d7e565bbe41a5e0c2299652e037b02180b2ba
Khan B <itisameerkhan@gmail.com>
2025-03-10 14:29:03 +0530

    exploring GET, POST, PATCH, DELETE

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,24 +1,59 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.listen(8080, () => {
   console.log("SERVER IS LISTENING TO PORT: 8080");
 });
 
-app.use((req, res) => {
-  res.send("hello from the server");
+// app.use("/", (req, res) => {
+//     res.send("HELLO FROM / ");
+//  });
+
+app.use("/api/helloworld", (req, res) => {
+  res.send("/API/HELLOWORLD");
 });
 
-
-
-app.get("/api/helloworld", (req, res) => {
-    res.json({
-        success: true,
-        message: "ok"
-    })
+app.get("/", (req, res) => {
+  res.send("GET /");
 });
 
-app.use("/api/helloworld",(req, res) => {
-    res.send("hello world");
+app.use("/api/test", (req, res) => {
+  res.send("/API/TEST");
+});
+
+app.post("/api/post/data", (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    success: true,
+    message: "POST call ok",
+    data: req.body,
+  });
+});
+
+app.get("/api/get/user/1", (req, res) => {
+  res.json({
+    success: true,
+    message: "ok",
+    data: {
+      firstName: "Ameer",
+      lastName: "Khan",
+    },
+  });
+});
+
+app.patch("/api/update/user/1", (req, res) => {
+  res.json({
+    success: true,
+    message: "data updated successfully",
+  });
+});
+
+app.delete("/api/delete/user/1", (req, res) => {
+  res.json({
+    success: true,
+    message: "data deleted successfully",
+  });
 });
